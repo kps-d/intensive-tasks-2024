d63ceee871ef306eeafa7b082674dbc6efea72df
kps-d <kuznecov.psd@gmail.com>
2024-11-20 02:02:29 +0300

    реализован метод isLeap.

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -25,12 +25,20 @@ package com.walking.intensive.chapter1.task3;
  */
 public class Task3 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
+        int year = 2025;
+        System.out.println(isLeap(year));
     }
 
     static boolean isLeap(int year) {
-        //Место для вашего кода
+        if (year % 400 == 0) {
+            return true;
+        }
 
-        return false; // Заглушка. При реализации - удалить
+        if (year % 100 == 0) {
+            return false;
+        }
+
+        return year % 4 == 0;
     }
 }
