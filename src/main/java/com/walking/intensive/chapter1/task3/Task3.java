5c585f509d3e0806b171877b579c8aa0e6dc2e12
kps-d <kuznecov.psd@gmail.com>
2024-11-20 20:57:31 +0300

    Реализовано одно булево выражение.

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -25,19 +25,11 @@ package com.walking.intensive.chapter1.task3;
  */
 public class Task3 {
     public static void main(String[] args) {
-        int year = 2025;
+        int year = 2024;
         System.out.println(isLeap(year));
     }
 
     static boolean isLeap(int year) {
-        if (year % 400 == 0) {
-            return true;
-        }
-
-        if (year % 100 == 0) {
-            return false;
-        }
-
-        return year % 4 == 0;
+        return (year % 400 == 0 || year % 100 != 0) && year % 4 == 0;
     }
 }
