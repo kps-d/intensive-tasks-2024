911a1e53872eb7d86c63613b679793fe8da64c12
Paul <kuznecov.psd@gmail.com>
2024-11-22 18:33:26 +0300

    Исправлены замечания.

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -25,9 +25,9 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
     public static void main(String[] args) {
 
-        double a = -1;
-        double b = 3;
-        double c = 4;
+        double a = 0;
+        double b = -2;
+        double c = 2;
 
         System.out.println(solveEquation(a, b, c));
     }
@@ -35,9 +35,6 @@ public class Task4 {
     static String solveEquation(double a, double b, double c) {
 
         double discriminant;
-        double root;
-        double rootOne;
-        double rootSecond;
 
         discriminant = (b * b) - 4 * a * c;
 
@@ -45,28 +42,32 @@ public class Task4 {
             return "Бесконечное множество решений.";
         }
 
-        if (discriminant < 0 || (a + b) == 0) {
+        if (discriminant < 0 || (a == 0 && b == 0)) {
             return "Количество решений: 0.";
         }
 
-        if (discriminant == 0 || a == 0) {
-            if (a == 0) {
-                root = -c / b;
-                return "Количество решений: 1. Корень: " + root;
-            }
-            root = -b / (2 * a);
-            return "Количество решений: 1. Корень: " + Math.abs(root);
+        if (discriminant == 0) {
+            return "Количество решений: 1. Корень: " + Math.abs(-b / (2 * a));
         }
 
+        double rootOne;
+        double rootSecond;
+
         if (discriminant > 0) {
+
+            if (a == 0) {
+                return "Количество решений: 1. Корень: " + (-c / b);
+            }
+
             rootOne = (-b - Math.sqrt(discriminant)) / (2 * a);
             rootSecond = (-b + Math.sqrt(discriminant)) / (2 * a);
+
             if (rootOne > rootSecond) {
                 return "Количество решений: 2. Корни: " + rootSecond + ";" + rootOne;
             }
             return "Количество решений: 2. Корни: " + rootOne + ";" + rootSecond;
         }
 
-        return null;
+        return "Ошибка ввода";
     }
 }
