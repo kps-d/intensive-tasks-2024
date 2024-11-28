f0240f0512aca21d1779e020c05f917a58b00fae
kps-d <kuznecov.psd@gmail.com>
2024-11-29 02:05:30 +0300

    работа над ошибками

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -128,7 +128,7 @@ public class Task5 {
         }
 
         double[] angles = new double[3];
-        angles[0] = Math.toDegrees(Math.acos((a * a + c * c - b * b) / (2 * a * c)));
+        angles[0] = getCos(a, b, c);
         angles[1] = Math.toDegrees(Math.acos((a * a + b * b - c * c) / (2 * a * b)));
         angles[2] = Math.toDegrees(Math.acos((b * b + c * c - a * a) / (2 * c * b)));
         Arrays.sort(angles);
@@ -197,5 +197,9 @@ public class Task5 {
     static double getHalfPerimeter(double a, double b, double c) {
 
         return (a + b + c) / 2;
+    }
+
+    static double getCos (double a, double b, double c) {
+        return Math.toDegrees(Math.acos((a * a + c * c - b * b) / (2 * a * c)));
     }
 }
